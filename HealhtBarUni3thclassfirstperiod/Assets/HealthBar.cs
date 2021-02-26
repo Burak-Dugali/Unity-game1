2f25b4371c412d080b76766c69a8bf5330200219
Burak Dugalı <burakdugali@outlook.com>
2021-02-26 10:42:03 +0300

    Health_Bar_Basic

---
+++ HealhtBarUni3thclassfirstperiod/Assets/HealthBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image PlayerHealthBar;
+    public float currenthealth = 0f;
+    public float minhealth = 0f;
+    public float maxhealth = 1f;
+    
+    public void Start()
+    {
+        //barin fill amount'unu unutma
+        currenthealth = minhealth;
+        PlayerHealthBar.fillAmount = minhealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Arttir()
+    {
+        currenthealth += 0.1f;
+        PlayerHealthBar.fillAmount += 0.1f;
+        if (currenthealth > 1f)
+        {
+            currenthealth = maxhealth;
+        }
+    }
+
+    public void Azalt()
+    {
+        currenthealth -= 0.1f;
+        PlayerHealthBar.fillAmount -= 0.1f;
+        if (currenthealth < 0)
+        {
+            currenthealth = minhealth;
+        }
+    }
+}
